40e9483ab66b1b5821775e1975e32b9d37e3acc2
butirpadi <butirpadi@gmail.com>
2022-01-04 08:03:57 +0800

    [UPDATE] add password char to password field

--- ORP/FormSetting.Designer.vb
+++ ORP/FormSetting.Designer.vb
@@ -37,11 +37,11 @@ Partial Class FormSetting
         Me.cbDocType = New System.Windows.Forms.ComboBox()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.tbAdminPassword = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
         Me.tbPrinter = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.btnTestPrint = New System.Windows.Forms.Button()
-        Me.tbAdminPassword = New System.Windows.Forms.TextBox()
-        Me.Label7 = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
@@ -49,72 +49,69 @@ Partial Class FormSetting
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(16, 31)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Location = New System.Drawing.Point(21, 38)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.Size = New System.Drawing.Size(101, 16)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Server Location"
         '
         'tbServer
         '
-        Me.tbServer.Location = New System.Drawing.Point(102, 29)
-        Me.tbServer.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.tbServer.Location = New System.Drawing.Point(136, 36)
+        Me.tbServer.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.tbServer.Name = "tbServer"
-        Me.tbServer.Size = New System.Drawing.Size(247, 20)
+        Me.tbServer.Size = New System.Drawing.Size(328, 22)
         Me.tbServer.TabIndex = 1
         '
         'tbUsername
         '
-        Me.tbUsername.Location = New System.Drawing.Point(102, 51)
-        Me.tbUsername.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.tbUsername.Location = New System.Drawing.Point(136, 63)
+        Me.tbUsername.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.tbUsername.Name = "tbUsername"
-        Me.tbUsername.Size = New System.Drawing.Size(247, 20)
+        Me.tbUsername.Size = New System.Drawing.Size(328, 22)
         Me.tbUsername.TabIndex = 3
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(16, 53)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Location = New System.Drawing.Point(21, 65)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.Size = New System.Drawing.Size(70, 16)
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "Username"
         '
         'tbPassword
         '
-        Me.tbPassword.Location = New System.Drawing.Point(102, 74)
-        Me.tbPassword.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.tbPassword.Location = New System.Drawing.Point(136, 91)
+        Me.tbPassword.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.tbPassword.Name = "tbPassword"
-        Me.tbPassword.Size = New System.Drawing.Size(247, 20)
+        Me.tbPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.tbPassword.Size = New System.Drawing.Size(328, 22)
         Me.tbPassword.TabIndex = 5
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(16, 77)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Location = New System.Drawing.Point(21, 95)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.Size = New System.Drawing.Size(67, 16)
         Me.Label3.TabIndex = 4
         Me.Label3.Text = "Password"
         '
         'tbDatabase
         '
-        Me.tbDatabase.Location = New System.Drawing.Point(102, 97)
-        Me.tbDatabase.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.tbDatabase.Location = New System.Drawing.Point(136, 119)
+        Me.tbDatabase.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.tbDatabase.Name = "tbDatabase"
-        Me.tbDatabase.Size = New System.Drawing.Size(247, 20)
+        Me.tbDatabase.Size = New System.Drawing.Size(328, 22)
         Me.tbDatabase.TabIndex = 7
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(16, 99)
-        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Location = New System.Drawing.Point(21, 122)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(84, 13)
+        Me.Label4.Size = New System.Drawing.Size(107, 16)
         Me.Label4.TabIndex = 6
         Me.Label4.Text = "Database Name"
         '
@@ -122,9 +119,10 @@ Partial Class FormSetting
         '
         Me.btnCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnCancel.Location = New System.Drawing.Point(302, 305)
+        Me.btnCancel.Location = New System.Drawing.Point(403, 375)
+        Me.btnCancel.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnCancel.Name = "btnCancel"
-        Me.btnCancel.Size = New System.Drawing.Size(75, 30)
+        Me.btnCancel.Size = New System.Drawing.Size(100, 37)
         Me.btnCancel.TabIndex = 10
         Me.btnCancel.Text = "Close"
         Me.btnCancel.UseVisualStyleBackColor = True
@@ -133,9 +131,10 @@ Partial Class FormSetting
         '
         Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnSave.Location = New System.Drawing.Point(220, 305)
+        Me.btnSave.Location = New System.Drawing.Point(293, 375)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnSave.Name = "btnSave"
-        Me.btnSave.Size = New System.Drawing.Size(75, 30)
+        Me.btnSave.Size = New System.Drawing.Size(100, 37)
         Me.btnSave.TabIndex = 11
         Me.btnSave.Text = "Save"
         Me.btnSave.UseVisualStyleBackColor = True
@@ -144,9 +143,10 @@ Partial Class FormSetting
         '
         Me.btnConnect.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnConnect.Location = New System.Drawing.Point(15, 305)
+        Me.btnConnect.Location = New System.Drawing.Point(20, 375)
+        Me.btnConnect.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnConnect.Name = "btnConnect"
-        Me.btnConnect.Size = New System.Drawing.Size(104, 30)
+        Me.btnConnect.Size = New System.Drawing.Size(139, 37)
         Me.btnConnect.TabIndex = 12
         Me.btnConnect.Text = "Test Connection"
         Me.btnConnect.UseVisualStyleBackColor = True
@@ -163,9 +163,11 @@ Partial Class FormSetting
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.Label4)
         Me.GroupBox1.Controls.Add(Me.tbPassword)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 15)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(365, 129)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(487, 159)
         Me.GroupBox1.TabIndex = 13
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Connection"
@@ -175,17 +177,19 @@ Partial Class FormSetting
         Me.cbDocType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cbDocType.FormattingEnabled = True
         Me.cbDocType.Items.AddRange(New Object() {"Sales Order", "Purchase Order", "Delivery Order", "Customer Invoice", "Vendor Bill"})
-        Me.cbDocType.Location = New System.Drawing.Point(113, 26)
+        Me.cbDocType.Location = New System.Drawing.Point(151, 32)
+        Me.cbDocType.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.cbDocType.Name = "cbDocType"
-        Me.cbDocType.Size = New System.Drawing.Size(236, 21)
+        Me.cbDocType.Size = New System.Drawing.Size(313, 24)
         Me.cbDocType.TabIndex = 15
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(16, 29)
+        Me.Label5.Location = New System.Drawing.Point(21, 36)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(93, 13)
+        Me.Label5.Size = New System.Drawing.Size(113, 16)
         Me.Label5.TabIndex = 14
         Me.Label5.Text = "Default Document"
         '
@@ -199,28 +203,47 @@ Partial Class FormSetting
         Me.GroupBox2.Controls.Add(Me.Label6)
         Me.GroupBox2.Controls.Add(Me.Label5)
         Me.GroupBox2.Controls.Add(Me.cbDocType)
-        Me.GroupBox2.Location = New System.Drawing.Point(12, 147)
+        Me.GroupBox2.Location = New System.Drawing.Point(16, 181)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(365, 136)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(487, 167)
         Me.GroupBox2.TabIndex = 16
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Application Setting"
         '
+        'tbAdminPassword
+        '
+        Me.tbAdminPassword.Location = New System.Drawing.Point(151, 97)
+        Me.tbAdminPassword.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.tbAdminPassword.Name = "tbAdminPassword"
+        Me.tbAdminPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.tbAdminPassword.Size = New System.Drawing.Size(313, 22)
+        Me.tbAdminPassword.TabIndex = 19
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(21, 101)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(108, 16)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Admin Password"
+        '
         'tbPrinter
         '
-        Me.tbPrinter.Location = New System.Drawing.Point(113, 53)
-        Me.tbPrinter.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.tbPrinter.Location = New System.Drawing.Point(151, 65)
+        Me.tbPrinter.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.tbPrinter.Name = "tbPrinter"
-        Me.tbPrinter.Size = New System.Drawing.Size(236, 20)
+        Me.tbPrinter.Size = New System.Drawing.Size(313, 22)
         Me.tbPrinter.TabIndex = 17
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(16, 56)
-        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Location = New System.Drawing.Point(21, 69)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(68, 13)
+        Me.Label6.Size = New System.Drawing.Size(85, 16)
         Me.Label6.TabIndex = 16
         Me.Label6.Text = "Printer Name"
         '
@@ -228,36 +251,19 @@ Partial Class FormSetting
         '
         Me.btnTestPrint.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.btnTestPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnTestPrint.Location = New System.Drawing.Point(126, 305)
+        Me.btnTestPrint.Location = New System.Drawing.Point(168, 375)
+        Me.btnTestPrint.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnTestPrint.Name = "btnTestPrint"
-        Me.btnTestPrint.Size = New System.Drawing.Size(87, 30)
+        Me.btnTestPrint.Size = New System.Drawing.Size(116, 37)
         Me.btnTestPrint.TabIndex = 17
         Me.btnTestPrint.Text = "Test Print"
         Me.btnTestPrint.UseVisualStyleBackColor = True
         '
-        'tbAdminPassword
-        '
-        Me.tbAdminPassword.Location = New System.Drawing.Point(113, 79)
-        Me.tbAdminPassword.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
-        Me.tbAdminPassword.Name = "tbAdminPassword"
-        Me.tbAdminPassword.Size = New System.Drawing.Size(236, 20)
-        Me.tbAdminPassword.TabIndex = 19
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(16, 82)
-        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(85, 13)
-        Me.Label7.TabIndex = 18
-        Me.Label7.Text = "Admin Password"
-        '
         'FormSetting
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(389, 347)
+        Me.ClientSize = New System.Drawing.Size(519, 427)
         Me.Controls.Add(Me.btnTestPrint)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
@@ -265,7 +271,7 @@ Partial Class FormSetting
         Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.btnCancel)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
-        Me.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "FormSetting"
